ba23a289989e9708535f0a51cc02b3e100596d7f
Fatih Sarikaya <fatihsarikaya4@hotmail.com>
2022-11-24 18:40:08 +0300

    Add files via upload

--- mobile-de-3.py
+++ mobile-de-3.py
@@ -92,23 +92,13 @@ def fonksiyon (i):
 
     sleep_time = 5
 
-#        options = Options()
-#        options = webdriver.ChromeOptions()
+    #options = webdriver.ChromeOptions()
+    #prefs = {"profile.managed_default_content_settings.images": 2}
+    #options.add_experimental_option("prefs", prefs)
+    #service = Service(executable_path = r'C:\Users\Fatih\Desktop\mobile-de\chromedriver.exe')
+    #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    #driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     
-    #options.headless = True
-    #options.add_argument("--window-size=1920,1080")
-    #options.add_argument("--headless")
-    #options.add_argument("--disable-gpu")
-    #options.add_argument("--no-sandbox")
-    
-#        prefs = {"profile.managed_default_content_settings.images": 2} # this is to not load images
-#        options.add_experimental_option("prefs", prefs)
-
-    #start a driver
-
-#        service = Service(executable_path = r'C:\Users\Fatih\Desktop\mobile-de\chromedriver.exe')
-#        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
     # Go to webpage and scrape data
 
     # Test for Audi 100
